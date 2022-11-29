406f1872a01f74f0ab61612eaf7289fe7eb242da
Diana Smirnova <dsmirnova@erply.com>
2022-11-29 14:16:29 +0200

    value

---
+++ lesson-1.js
@@ -0,0 +1,14 @@
+let numberType = 123456;
+let stringType = 'IT School \'Hillel\'';
+let boolType=false;
+
+
+console.log('value:',numberType,'type:',typeof numberType);
+// expected output: "number"
+
+console.log('value:',stringType,'type:',typeof stringType);
+// expected output: "string"
+
+console.log(boolType,typeof boolType);
+// expected output: "boolean"
+
