168861febc69b10b12a0d069ef9ead65358ee516
Diana Smirnova <dsmirnova@erply.com>
2022-11-29 22:26:34 +0200

    value

--- lesson-1.js
+++ lesson-1.js
@@ -1,3 +1,4 @@
+// Option-1
 let numberType = 123456;
 let stringType = 'IT School \'Hillel\'';
 let boolType=false;
@@ -12,3 +13,15 @@ console.log('value:',stringType,'type:',typeof stringType);
 console.log(boolType,typeof boolType);
 // expected output: "boolean"
 
+//Option-2 (function)
+//DRY I don't  repeat a code, but for this need to array and loop.
+function types(value) {
+    console.log('value:',value,'type:',typeof value);
+}
+types(numberType);
+types(stringType);
+types(boolType);
+
+function myFunction() {
+    alert('IT School \'Hillel\'');
+}
