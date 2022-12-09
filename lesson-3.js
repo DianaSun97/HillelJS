1d86123351d4724d7874486394f5054fdd4ee3dd
Diana Smirnova <dianaart1997@gmail.com>
2022-12-09 16:22:02 +0200

    lesson 3

---
+++ lesson-3.js
@@ -0,0 +1,8 @@
+let name = +prompt('Your name?');
+
+for (var i=0; i<2; i++){
+    console.log(i+"Happy birthday to you")
+    if (name === 2) {
+        console.log(`Happy birthday, dear ${name}`);}
+    console.log("Happy birthday to you")
+}
