6c67893e573702d83f5ae4efc59fbef7b940f1f7
dianasun <dianaart1997@gmail.com>
2022-12-22 19:56:05 +0200

    Home work1

--- lesson-7.js
+++ lesson-7.js
@@ -15,4 +15,43 @@ const jQ = frameworks[indexDelete]
 frameworks.splice(indexDelete, 1)
 console.log(frameworks, `${jQ} Это здесь лишнее`)
 
+let result = removeNegativeElements([-9, 2, 3, 0, -28, 'value']); // [2, 3, 0, 'value'];
+// let result = removeNegativeElements([-9, -21, -12]); // []
+// let result = removeNegativeElements(['-102', 102]); // ['-102', 102]
+
+// function removeNegativeElements(result) {
+//     return result.filter(function(item) {
+//         return typeof item === 'number' && item >= 0;
+//     });
+// }
+//
+function removeNegativeElements(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number' && arr[i] >= 0) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+ console.log(removeNegativeElements(result))
+//
+let arr = [1 , true , 42 , "red" , 64 , "green" , "web" , new Date() , -898 , false]
+// let result = getStringElements(arr); // ['green', 'web']
+// // function getStringElements(arr) {
+// //     return arr.filter(element => typeof element === 'string');
+// // }
+
+console.log(getStringElements(arr))
+
+function getStringElements(arr) {
+    let resultStr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'string') {
+            resultStr.push(arr[i]);
+        }
+    }
+    return resultStr;
+}
+
 
