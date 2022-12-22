c85a332f1473b82934e4af6028468a95f7695f43
dianasun <dianaart1997@gmail.com>
2022-12-22 19:59:29 +0200

    Home work1

--- lesson-7.js
+++ lesson-7.js
@@ -1,3 +1,5 @@
+
+//Task 1
 let frameworks = ['AngularJS', 'jQuery']
 //option 1
 // frameworks = ['Backbone.js', ...frameworks]
@@ -15,16 +17,21 @@ const jQ = frameworks[indexDelete]
 frameworks.splice(indexDelete, 1)
 console.log(frameworks, `${jQ} Это здесь лишнее`)
 
+
+//Task 2
 let result = removeNegativeElements([-9, 2, 3, 0, -28, 'value']); // [2, 3, 0, 'value'];
 // let result = removeNegativeElements([-9, -21, -12]); // []
 // let result = removeNegativeElements(['-102', 102]); // ['-102', 102]
 
+//Option1
 // function removeNegativeElements(result) {
 //     return result.filter(function(item) {
 //         return typeof item === 'number' && item >= 0;
 //     });
 // }
 //
+
+//Option2
 function removeNegativeElements(arr) {
     let result = [];
     for (let i = 0; i < arr.length; i++) {
@@ -35,15 +42,18 @@ function removeNegativeElements(arr) {
     return result;
 }
  console.log(removeNegativeElements(result))
-//
+
+
+//Task1
+//Option 1
 let arr = [1 , true , 42 , "red" , 64 , "green" , "web" , new Date() , -898 , false]
 // let result = getStringElements(arr); // ['green', 'web']
 // // function getStringElements(arr) {
 // //     return arr.filter(element => typeof element === 'string');
 // // }
 
-console.log(getStringElements(arr))
 
+//Option2
 function getStringElements(arr) {
     let resultStr = [];
     for (let i = 0; i < arr.length; i++) {
@@ -54,4 +64,4 @@ function getStringElements(arr) {
     return resultStr;
 }
 
-
+console.log(getStringElements(arr))
