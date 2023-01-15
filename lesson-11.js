49fe02c2872c1d31c7d6c1bb124ba14d09372279
dianasun <dianaart1997@gmail.com>
2023-01-15 19:57:22 +0200

    Home work11

--- lesson-11.js
+++ lesson-11.js
@@ -1,67 +1,84 @@
-console.log(Data);
-const results = JSON.parse(Data);
-console.log(results);
-console.log(results.results);
+
 
 //html
+const bodySpace = document.getElementsByTagName('body');
 let selected = document.querySelector('.selected');
-let buttons = document.getElementsByClassName('button');
-let buttonDelete = document.getElementsByClassName('buttonDelete');
-let selectedMain = document.querySelector('.selectedMain');
+const personagesTable = document.getElementById('personages-list');
 let divs = document.createElement("div");
 let back = document.querySelector('.back');
 divs.classList.add('selectedBlock');
 
+bodySpace[0].addEventListener('click', () => {
+    selected.textContent = '';
+});
 
-let objects = [...Object.values(results.results)]
 
-let res = {
-    id: '',
-    name: '',
-    status: ''
-};
-
-objects.find(index => {
-    if (index.id === 1 && index.name === 'Rick Sanchez' && index.status === 'Alive') {
-        res.id = index.id;
-        res.name = index.name;
-        res.status = index.status;
+function handleName(event) {
+    const div = event.target;
+    console.log(div)
+    let name = div.getAttribute('div-name');
+    if (name === null) {
+        name = div.parentElement.getAttribute('div-name');
+        selected.textContent = `Selected: ${name}`;
+    } else {
+        selected.textContent = `Selected: ${name}`;
     }
-})
+    event.stopPropagation();
+}
 
 
-for (let key of buttons) {
-    key.addEventListener('click', (e) => {
-        let resAttribute = e.target.getAttribute('btn-name');
 
-        if (resAttribute === null) {
-            selected.textContent = 'Selected: - - - - -';
-        } else {
-            selected.textContent = `Selected: ${res[resAttribute]}`;
-        }
+//delete
+function handleDelete(event) {
+    event.stopPropagation();
+    const buttons = event.target;
+    const div = buttons.parentElement;
+    div.remove()
+}
+
+
+function personagesList(data) {
+    console.log(data)
+    data.forEach(val => {
+        let div = document.createElement('div');
+
+        let div1 = document.createElement('div');
+        let div2 = document.createElement('div');
+        let div3 = document.createElement('div');
+        let buttons = document.createElement('button');
+
+        div1.innerText = 'id: ' + val.id;
+        div2.innerText = 'name: ' + val.name;
+        div3.innerText = 'status: ' + val.status;
+
+        buttons.onclick = handleDelete;
+        div.onclick = handleName;
+        div.setAttribute('div-name', val.name);
+        div.classList.add('divItem');
+
+        buttons.innerHTML = 'delete';
+        buttons.setAttribute('btn-name', 'delete');
+
+        div.appendChild(div1);
+        div.appendChild(div2);
+        div.appendChild(div3);
+        div.appendChild(buttons);
+
+
+        personagesTable.appendChild(div);
+    });
+}
+
+const response = JSON.parse(Data);
+personagesList(response.results);
+
+//back
+back.addEventListener('click', (e) => {
+    e.stopPropagation()
+    // location.reload()
+    document.querySelectorAll('.divItem').forEach(value => {
+        value.remove()
     })
-}
+    personagesList(response.results);
+});
 
-const selectedDelete = {
-    delete: (element) => {
-        element.remove();
-    }
-}
-
-for (let i of buttonDelete) {
-    i.addEventListener('click', (e) => {
-        let listResult = e.target;
-        let record = listResult.getAttribute('btn-name');
-        if (record in selectedDelete) {
-            console.log(selectedDelete[record](listResult.parentElement));
-        }
-    })
-}
-
-function deletes(e) {
-    let listResult = e.target;
-    let record = listResult.getAttribute('btn-name');
-    if (record in listResult) {
-        console.log(listResult[record](listResult.parentElement));
-    }
-}
