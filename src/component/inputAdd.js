9d07432740bbc4cc088f73c9aab1901c467c76a9
dianasun <dianaart1997@gmail.com>
2023-04-10 21:22:54 +0300

    add components

--- src/component/inputAdd.js
+++ src/component/inputAdd.js
@@ -1,17 +1,50 @@
 import useAddRecords from "../hooks/useAddRecords";
+import {useState} from "react";
 
 const AddRecordForm = () => {
     const {addRecord, data, loading, error} = useAddRecords();
-
+    const [formValue, setFormValue] = useState({
+        "name": `User-${Math.round(100*Math.random())}`,
+        "surname": "wild",
+        "phone": `+380${Math.round(100000000*Math.random())}`
+    });
     const addRecordHandler = () => {
-        addRecord({
-            "name": `User-${Math.round(100*Math.random())}`,
-            "surname": "wild",
-            "phone": `+380${Math.round(100000000*Math.random())}`
-        });
+        addRecord(formValue);
     }
 
+    const handleEdit = (event) => {
+        const { name, value } = event.target;
+        setFormValue((prevState) => {
+            return {
+                ...prevState,
+                [name]: value,
+            };
+        });
+    };
+
+    const { name, surname, phone } = formValue;
+
     return (<>
+    <input
+        type="text"
+        name="name"
+        placeholder="Your name"
+        onChange={handleEdit}
+        value={name} />
+    <br />
+    <input
+        type="text"
+        name="surname"
+        placeholder="Your surname"
+        onChange={handleEdit}
+        value={surname} />
+    <br />
+    <input
+        type="number"
+        name="phone"
+        placeholder="Your phone"
+        onChange={handleEdit}
+        value={phone} />
         <button onClick={addRecordHandler}>Add record</button>
     </>)
 }
